42efcd7a16ed3af7a8c211e3ff50d1cbdba7d5b7
Jason Davies <jason@jasondavies.com>
2013-03-21 21:47:12 +0000

    d3.geo.bounds: allow bounds to cross antimeridian.
    λ0 > λ1 indicates the bounds cross the antimeridian.  This makes sense
    in spherical coordinates, since the intuitive way to generate a polygon
    representing the bounds still works.
    
    See #1154.  Remaining: adjust φ-extent for great arcs.

--- src/geo/bounds.js
+++ src/geo/bounds.js
@@ -6,7 +6,9 @@ import "stream";
 d3.geo.bounds = d3_geo_bounds(d3_identity);
 
 function d3_geo_bounds(projectStream) {
-  var x0, y0, x1, y1;
+  var x0, y0, x1, y1, // bounds
+      x_, // previous x-coordinate
+      invert; // x1 < x0
 
   var bound = {
     point: boundPoint,
@@ -14,15 +16,27 @@ function d3_geo_bounds(projectStream) {
     lineEnd: d3_noop,
 
     // While inside a polygon, ignore points in holes.
+    // TODO if area > hemisphere then bounds are whole sphere?
     polygonStart: function() { bound.lineEnd = boundPolygonLineEnd; },
     polygonEnd: function() { bound.point = boundPoint; }
   };
 
   function boundPoint(x, y) {
-    if (x < x0) x0 = x;
-    if (x > x1) x1 = x;
+    if (!invert && (invert = Math.abs(x - x_) > 180)) {
+      if (x < x0) x1 = x;
+      else x0 = x;
+    } else if (invert) {
+      if (x1 < x && x < x0) {
+        if (x - x1 > x - x0) x1 = x;
+        else x0 = x;
+      }
+    } else {
+      if (x < x0) x0 = x;
+      if (x > x1) x1 = x;
+    }
     if (y < y0) y0 = y;
     if (y > y1) y1 = y;
+    x_ = x;
   }
 
   function boundPolygonLineEnd() {
@@ -31,6 +45,8 @@ function d3_geo_bounds(projectStream) {
 
   return function(feature) {
     y1 = x1 = -(x0 = y0 = Infinity);
+    x_ = NaN;
+    invert = false;
     d3.geo.stream(feature, projectStream(bound));
     return [[x0, y0], [x1, y1]];
   };
